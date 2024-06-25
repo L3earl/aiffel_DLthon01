eaaba01f9498c65e0fb9ad26860b5228a0929e43
L3earl <sainknight87@gmail.com>
2024-06-26 08:11:27 +0900

    1

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 #%% 라이브러리 로드
 import os
 import pandas as pd
+import numpy as np
 import json
 import re
 import torch
 from sklearn.model_selection import train_test_split
-# from transformers import AutoTokenizer, AutoModelForSequenceClassification, AdamW, get_scheduler
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, get_scheduler
 from torch.utils.data import DataLoader, Dataset
 import wandb
@@ -17,6 +17,7 @@ import logging
 import warnings
 from konlpy.tag import Okt
 from soynlp.normalizer import repeat_normalize
+import torch.nn.functional as F
 
 #%% 전역 변수 
 # W&B 로그인
@@ -131,12 +132,13 @@ test_data.reset_index(drop=True, inplace=True)
 exp_set = pd.read_csv('./data/exp_set.csv')
 
 #%% 모델 학습
-train_texts = concat_tr_df['conversation'].to_list()
-train_labels = concat_tr_df['encoded_label'].to_list()
-
 tmpDf = pd.DataFrame()
 
 for idx, row in exp_set.iterrows():
+    train_texts = concat_tr_df['conversation'].to_list()
+    train_labels = concat_tr_df['encoded_label'].to_list()
+    test_texts = test_data['text'].tolist()
+
     # 실험 셋에서 하이퍼파라미터 및 전처리 함수 로드
     pre = [None, None, None]  # pre01, pre02, pre03에 대한 함수를 저장할 리스트 초기화
     for i in range(3):  # pre01~n 반복
@@ -164,25 +166,29 @@ for idx, row in exp_set.iterrows():
         "batch_size": batch_size
     }
     
-    # wandb에 기록될 실험 이름 
-    exp_name = f"{today_date}_{model_name}"
-    
     # 전처리 목록에 clean 함수가 있으면 적용
     preprocess_func = next((func for func in pre if func is not None and func.__name__ == 'clean'), None)
     if preprocess_func:
         print('전처리 함수 적용')
-        # train_texts = [preprocess_func(sentence) for sentence in train_texts]
         train_texts = list(map(clean, train_texts))
-        test_texts = list(map(clean, test_data))
+        test_texts = list(map(clean, test_texts))
+        clean_YN = 'Y'
+    else:
+        clean_YN = 'N'
         
     # 전처리 목록에 remove_stopwords 함수가 있으면 적용
     preprocess_func = next((func for func in pre if func is not None and func.__name__ == 'remove_stopwords'), None)
     if preprocess_func:
         print('불용어 제거')
-        # train_texts = [preprocess_func(sentence, stopwords, okt) for sentence in train_texts]
         train_texts = preprocess_func(train_texts, stopwords, okt)
-        test_texts = preprocess_func(test_data, stopwords, okt)
-        
+        test_texts = preprocess_func(test_texts, stopwords, okt)
+        stopwords_YN = 'Y'
+    else:
+        stopwords_YN = 'N'
+    
+    # wandb에 기록될 실험 이름 
+    exp_name = f"{today_date}_{model_name.replace('/', '_').replace('-', '_')}_{clean_YN}_{stopwords_YN}_{epochs}_{batch_size}"
+    
     # 데이터 분할
     train_texts, val_texts, train_labels, val_labels = train_test_split(
         train_texts, train_labels, test_size=test_size, random_state=seed, stratify=train_labels
@@ -193,6 +199,7 @@ for idx, row in exp_set.iterrows():
 
     train_encodings = tokenizer(train_texts, truncation=True, padding=True, return_tensors='pt')
     val_encodings = tokenizer(val_texts, truncation=True, padding=True, return_tensors='pt')
+    test_encodings = tokenizer(test_texts, truncation=True, padding=True, return_tensors='pt')
 
     train_dataset = CustomDataset(train_encodings, train_labels)
     val_dataset = CustomDataset(val_encodings, val_labels)
@@ -219,6 +226,11 @@ for idx, row in exp_set.iterrows():
     # W&B에 모델을 로깅할 수 있도록 W&B에 등록
     wandb.watch(model, log="all")
 
+    # early stopping을 위한 변수 초기화
+    best_val_loss = np.inf
+    patience = 5  # 성능이 개선되지 않는 에포크의 최대 허용 횟수
+    no_improve_epoch = 0
+    
     # 학습 루프
     for epoch in range(epochs):
         model.train()
@@ -277,6 +289,21 @@ for idx, row in exp_set.iterrows():
             "val_accuracy": val_accuracy,
             "val_f1_score": final_score["f1"]
         })
+        
+        # early stopping
+        # 성능 개선 확인
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            no_improve_epoch = 0
+            # 모델 저장 로직 추가 가능
+            torch.save(model.state_dict(), f'./torch_models/{exp_name}.pth')
+        else:
+            no_improve_epoch += 1
+
+        # 조기 중단 조건 확인
+        if no_improve_epoch > patience:
+            print(f'Early stopping triggered after {epoch + 1} epochs.')
+            break
     
     # 실험 결과를 DataFrame으로 변환하여 저장
     tmp = exp_set.iloc[idx]
@@ -295,17 +322,48 @@ for idx, row in exp_set.iterrows():
 
     # wandb 종료
     wandb.finish()
+    
+    # 모델 예측
+    # 입력 데이터를 디바이스로 이동
+    test_encodings = {key: val.to(device) for key, val in test_encodings.items()}
+
+    # 모델을 평가 모드로 전환
+    model.eval()
+
+    # 예측 수행
+    with torch.no_grad():
+        outputs = model(**test_encodings)
+
+    # 로짓(logits)에서 예측 확률 계산
+    predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    
+    # 확률 파일 저장 - 추후 앙상블에 사용
+    predictions_np = predictions.cpu().numpy()
+    predictions_df = pd.DataFrame(predictions_np, columns=['갈취 대화','기타 괴롭힘 대화','직장 내 괴롭힘 대화','협박 대화'])
+    # predictions_df['max_val'] = predictions_df.max(axis=1)
+    predictions_df['label'] = predictions_df.iloc[:, :4].apply(lambda row: row.argmax(), axis=1)
+    predictions_df['class'] = predictions_df.idxmax(axis=1)
+
+    predictions_df.to_csv(f'./test_pred_logit/{exp_name}.csv', index=False, encoding='utf-8-sig')
+
+    # 예측 결과 확인
+    predicted_classes = torch.argmax(predictions, dim=1)
+
+    # 텐서를 CPU로 이동시키고 NumPy 배열로 변환
+    predicted_classes_np = predicted_classes.cpu().numpy()
+
+    submission = pd.read_csv('./data/new_submission.csv')
+    submission['class'] = predicted_classes_np
+
+    # 제출 파일 저장
+    submission.to_csv(f'./test_pred_clss/{exp_name}.csv', index=False)
 
 # DataFrame을 CSV 파일로 저장
-tmpDf.to_csv('./data/results.csv', index=False)
-
-#%% 모델 저장
-
-
+result_csv = pd.read_csv('./data/results.csv')
+result_csv = pd.concat([result_csv, tmpDf], ignore_index=True)
+result_csv.to_csv('./data/results.csv', index=False)
 
 
-#%% 모델 평가
 
 
-#%% 모델 예측
 
